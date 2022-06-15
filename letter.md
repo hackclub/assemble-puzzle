418cd9a0d1e747d91be0d3a849ae88f255f72c93
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-06-15 17:13:51 -0600

    remove letter from client build

---
+++ letter.md
@@ -0,0 +1,15 @@
+![The Golden Gate Bridge at night.](gzooks_secret "📍 San Francisco, CA")
+
+Dear Hack Clubber,
+
+You found us, haven’t you? You probably had to poke around to get here. Maybe you saw the clues immediately. Maybe it took some time. Perhaps you even had help from someone.
+
+Now that you’ve made it please don’t spoil the fun! You can share that you got our message and our postcard above, but the content of this letter is just between us. If you still want to be involved, you should offer a hand in the Slack and share your wisdom with someone looking for help to also reach this page.
+
+With that out of the way, we’re super excited to share some news with you: we're organizing Assemble, **an IRL hackathon in San Francisco from 5th-7th of August**. Around 150 attendees will join us– fellow Hack Clubbers, new high schoolers outside the community, and [some previous AMA guests](https://hackclub.com/amas#past-amas).
+
+We're still locking in the final logistics before we share this publicly, so stay tuned for the official post going out in the{' '} [#announcements](https://app.slack.com/client/T0266FRGM/C0266FRGT) channel late next week.
+
+Thank you so much for finding us. We’ll be in touch,
+
+\- The Assemble Team
--- pages/secret.js
+++ pages/secret.js
@@ -1,69 +1,41 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import mdLetter from '../letter.md'
+import ReactMarkdown from 'react-markdown'
 
 const secret = require("jsonwebtoken");
 const privateKey =
   "ginKjqrRLtrvEzgRs7s3dT5J70ZXTbb8j0EGJeCZrtH5Ekz4gyQQkNBVpExv";
 
-export default function Secret({ jwt }) {
-  let hasAccess = false;
-  try {
-    if (jwt.endsWith(".")) {
-      jwt = jwt.slice(0, -1);
-    }
-    if (jwt.split(".").length <= 2) {
-      if (JSON.parse(atob(jwt.split(".")[0])).alg == "none") {
-        hasAccess = JSON.parse(atob(jwt.split(".")[1])).hasAccess;
-      }
-    } else {
-      hasAccess = false;
-    }
-  } catch (err) {
-    hasAccess = false;
-  }
+// Use a custom renderer so that we can include image in Markdown and avoid anything being leaked
+const renderers = {
+  img: ({
+      alt,
+      src,
+      title,
+  }) => (
+    <a href={`/${src}.jpeg`} className='decoration-none'>
+      <figure>
+        {/* TODO: Add everyone's signature ontop of the photo */}
+        <img src={`/${src}.png`} alt={alt} />
+        <figcaption>{title}</figcaption>
+      </figure>
+    </a>
+  ),
+};
+
+export default function Secret({ hasAccess, letterContent }) {
+  // Doing this to avoid Next hydration error from md component, which shouldn't be happening
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
   if (hasAccess) {
-    return (
+    return loaded ? (
       <div id="text-container" class="shadow">
-        <a href="./gzooks.jpeg" className='decoration-none'>
-          <figure>
-            {/* TODO: Add everyone's signature ontop of the photo */}
-            <img src="./gadzooks.png" alt="The Golden Gate Bridge at night." />
-            <figcaption>📍 San Francisco, CA</figcaption>
-          </figure>
-        </a>
-        <p>
-          Dear Hack Clubber,
-        </p>
-        <p>
-          You found us, haven’t you? You probably had to poke around to get
-          here. Maybe you saw the clues immediately. Maybe it took some time.
-          Perhaps you even had help from someone.
-        </p>
-        <p>
-          Now that you’ve made it please don’t spoil the fun! You can share
-          that you got our message and our postcard above, but the content of
-          this letter is just between us.  If you still want to be involved, you
-          should offer a hand in the Slack and share your wisdom with someone
-          looking for help to also reach this page.
-        </p>
-        <p>
-          With that out of the way, we’re super excited to share some news with
-          you: we're organizing Assemble, <strong>an IRL hackathon in San
-          Francisco from 5th-7th of August</strong>. Around 150 attendees will
-          join us– fellow Hack Clubbers, new high schoolers outside the
-          community, and <a href="https://hackclub.com/amas#past-amas">some
-          previous AMA guests</a>.
-        </p>
-        <p>We're still locking in the final logistics before we share this
-        publicly, so stay tuned for the official post going out in the{' '}
-        <a href="https://app.slack.com/client/T0266FRGM/C0266FRGT">#announcements</a> channel late next week.</p>
-        <p>
-          Thank you so much for finding us. We’ll be in touch,
-        </p>
-        <p>
-          - The Assemble Team
-        </p>
+        <ReactMarkdown components={renderers} children={letterContent} />
       </div>
-    );
+    ) : 'gadzooks';
   } else {
     useEffect(() => {
       setTimeout(() => alert("ACCESS DENIED"), 200)
@@ -89,7 +61,28 @@ export default function Secret({ jwt }) {
   }
 }
 
-Secret.getInitialProps = async (context) => {
-  const jwt = context.query.jwt || "";
-  return { jwt };
-};
+export function getServerSideProps (ctx) {
+  let jwt = ctx.query.jwt || "";
+  let hasAccess = false;
+  try {
+    if (jwt.endsWith(".")) {
+      jwt = jwt.slice(0, -1);
+    }
+    if (jwt.split(".").length <= 2) {
+      if (JSON.parse(atob(jwt.split(".")[0])).alg == "none") {
+        hasAccess = JSON.parse(atob(jwt.split(".")[1])).hasAccess;
+      }
+    } else {
+      hasAccess = false;
+    }
+  } catch (err) {
+    hasAccess = false;
+  }
+
+  return {
+    props: {
+      hasAccess,
+      letterContent: hasAccess ? mdLetter : '',
+    },
+  }
+}
